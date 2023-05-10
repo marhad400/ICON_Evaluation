bf5b74b4c27228fb674e98d6f3f49faaff34ee87
marhad400 <markhaddad03@gmail.com>
2023-05-10 00:50:45 -0700

    Finished C1, C2 needs fixing

--- C1/game_of_life.py
+++ C1/game_of_life.py
@@ -18,10 +18,20 @@ class Game:
         board = np.copy(self._board)
         
         ''' Insert your code for updating the board based on the rules below '''
-
-
-
-        
+        for i in range(self._board.shape[0]):
+            for j in range(self._board.shape[1]):
+                # Get the sum of alive neighbors
+                neighbors = np.sum(self._board[i-1:i+2, j-1:j+2]) - self._board[i][j]
+                
+                # Game logic
+                if self._board[i][j] == 1 and (neighbors < 2 or neighbors > 3):
+                    board[i][j] = 0
+                elif self._board[i][j] == 1 and (2 <= neighbors <= 3):
+                    board[i][j] = 1
+                else:
+                    if neighbors == 3 and self._board[i][j] == 0:
+                        board[i][j] = 1
+                    
         self._board = board
 
 
--- C2/matrix.py
+++ C2/matrix.py
@@ -12,13 +12,33 @@ class MatrixOps:
     def largest_index(self, matrix):
         ''' Make this function return a tuple of the (row, col) 
             index of the largest value in the matrix '''
+        largest = self._matrix[0][0]
+        for i in range(self._matrix.shape[0]):
+            for j in range(self._matrix.shape[1]):
+                if largest < self._matrix[i][j]:
+                    largest = self._matrix[i][j]
+                    row_largest = i
+                    col_largest = j
 
-        return (0,0)
+        return (row_largest,col_largest)
+    
 
     def convolve(self, kernel, matrix):
         ''' Make this function return the result of a 2D convolution '''
+        padded_matrix = np.pad(matrix, pad_width=1, mode='constant', constant_values=0)
+        # output_dimension = (input_dim + 2*padding - kernel_dim) / (stride) + 1
+        output_height = (10 + 2 - 3) // 1 + 1
+        output_width = (10 + 2 - 3) // 1 + 1
+        # turns out to be dimensions of original matrix when padded with 1 layer of zeros
 
-        return matrix
+        output_matrix = np.zeros_like(matrix)
+
+        for row in range(1, output_height):
+            for col in range(1, output_width):
+                submatrix = padded_matrix[row-1:row+2, col-1:col+2]
+                output_matrix[row][col] = np.sum(np.multiply(submatrix, kernel))
+
+        return output_matrix
 
     def run(self):
         print("Largest index is at ", self.largest_index(self._matrix))
